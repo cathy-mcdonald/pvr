07e57bda6a177f90fb0f779a20c8dbbf11960de5
Cathy McDonald <github.cathy_mcdonald@spamgourmet.com>
2019-09-23 10:38:54 +1000

    Check if python script is already running before processing ts files

--- AllRecordedTS.py
+++ AllRecordedTS.py
@@ -1,12 +1,24 @@
 import sys
 import glob
+import psutil
+import os
 import ConvertRecordedTS
 
 def convert_all_recorded_ts(pvr_dir):
+  # Process all ts files in TS directory
   ts_dir = pvr_dir + "\\TS"
   file_list = glob.glob(ts_dir + "\\*.ts")
   for file_path in file_list:
     ConvertRecordedTS.convert_recorded_ts(pvr_dir, file_path, pvr_dir + "\\convert\\ConversionLog.txt")
 
 if __name__ == "__main__":
+  # Check if this script is already running
+  for proc in psutil.process_iter():
+    try:
+      if (proc.name().lower() == "python.exe") and ("AllRecordedTS.py" in proc.cmdline()) and (proc.pid != os.getpid()):
+        print("Script already running!")
+        exit()
+    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+      pass
+  
   convert_all_recorded_ts(sys.argv[1])
